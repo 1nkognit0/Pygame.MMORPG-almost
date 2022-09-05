fb556d322233b2ff70c2eb14f8628c395c3203a2
1nkognit0 <94606662+1nkognit0@users.noreply.github.com>
2022-09-05 20:30:42 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,6 +5,21 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 800
 SIZE = (WIDTH, HEIGHT)
-screen = pygame.display.set_mode(SIZE)
+screen = pygame.display.set_mode(SIZE, pygame.DOUBLEBUF)
 FPS = 120
 clock = pygame.time.Clock()
+tile_width = tile_height = 75
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+boxes_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+enemies_group = pygame.sprite.Group()
+attack_group = pygame.sprite.Group()
+ground_group = pygame.sprite.Group()
+border_group = pygame.sprite.Group()
+portal_group = pygame.sprite.Group()
+interface_group = pygame.sprite.Group()
+items_group = pygame.sprite.Group()
+NPC_group = pygame.sprite.Group()
+groups = [all_sprites, boxes_group, player_group, enemies_group, ground_group, border_group, portal_group, attack_group]
